ac53a376741a7fba026f4c9a812e7d9c8c758864
headrun <amruthab@headrun.com>
2018-12-12 17:24:18 +0530

    Adding Hacker Threads files

---
+++ subproject_posts/Phase_02/hacker_threads/configuration.py
@@ -0,0 +1,47 @@
+
+
+tables = {
+    'bleeping_computer': {
+        'posts': 'bleeping_posts',
+        'authors': 'bleeping_authors',
+        'crawl': 'bleeping_crawl'
+    },
+    'exe_tools': {
+        'posts': 'exe_posts',
+        'authors': 'exe_authors',
+        'crawl': 'exe_crawl'
+    },
+    'test': {
+        'posts': 'test_posts',
+        'authors': 'test_authors',
+        'crawl': 'test_crawl'
+    },
+    'kernel_mode': {
+        'posts': 'kernel_posts',
+        'authors': 'kernel_authors',
+        'crawl': 'kernel_crawl'
+    },
+    'wilder_security':{
+        'posts': 'wilder_posts',
+        'authors': 'wilder_authors',
+        'crawl': 'wilder_crawl'
+    },
+    'malware_bytes': {
+        'posts': 'mb_posts',
+        'authors': 'mb_authors',
+        'crawl': 'mb_crawl'
+    },
+    'hacker_threads':{
+        'posts': 'hackerthreads_posts',
+        'authors':'hackerthreads_authors',
+        'crawl':'hackerthreads_crawl'
+    },
+
+    'hack_port' : {
+        'posts': 'hp_posts',
+        'authors':'hp_authors',
+        'crawl':'hp_crawl',
+    },
+
+
+}

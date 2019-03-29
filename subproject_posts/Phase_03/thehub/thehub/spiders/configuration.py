42964dac8b135c687cfb0918ba25919dfd72d081
root <root@mlwr-fetcher-dev-0.cncff3kcowgubbtap0xpqnl5dg.bx.internal.cloudapp.net>
2019-03-29 07:58:05 +0000

    thehub

---
+++ subproject_posts/Phase_03/thehub/thehub/spiders/configuration.py
@@ -0,0 +1,11 @@
+tables = {
+        'thehub_tor': {
+         
+        'post_crawl': 'thehub_status',
+        'posts':'thehub_posts',
+	'author_crawl':'thehub_crawl',
+	'authors' : 'thehub_authors'
+        }
+
+}
+

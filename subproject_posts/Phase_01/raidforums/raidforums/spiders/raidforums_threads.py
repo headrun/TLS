f022253e32b8017e29c66374c9ffa95f371f0e19
saaiprakash1 <saiprakash@headrun.com>
2019-10-17 12:45:22 +0000

    [TLS-350]

--- subproject_posts/Phase_01/raidforums/raidforums/spiders/raidforums_threads.py
+++ subproject_posts/Phase_01/raidforums/raidforums/spiders/raidforums_threads.py
@@ -14,7 +14,7 @@ class Raidforums(scrapy.Spider):
     start_urls = ['https://raidforums.com/']
 
     def __init__(self):
-        self.conn = MySQLdb.connect(db="posts", host="localhost", user="tls_dev", passwd="hdrn!", use_unicode=True, charset="utf8")
+        self.conn = MySQLdb.connect(db="posts", host="localhost", user="root", passwd="qwe123", use_unicode=True, charset="utf8")
         self.cursor=self.conn.cursor()
         dispatcher.connect(self.close_conn, signals.spider_closed)
 
@@ -23,18 +23,18 @@ class Raidforums(scrapy.Spider):
         self.conn.close()
 
     def parse(self, response):
-        nodes = response.xpath('//tr[@class="forumstyles"]') 
-        for node in nodes:
-            link = ''.join(node.xpath('.//strong/a/@href').extract())
+        nodes = response.xpath('//div[@id="tab11"]//a[@class="forums__forum-name"]/@href').extract() 
+        for link in nodes:
             if 'https:/' not in link:link = 'https://raidforums.com/'+link
             yield Request(link,callback = self.parse_forum)
 
     def parse_forum(self,response):
-        n_page = response.xpath('//div[@class="pagination"]//a[@class="pagination_next"]//@href').extract()
+        n_page = response.xpath('//a[@class="pagination_next"]/@href').extract_first()
         if n_page:
-            if 'https:/' not in n_page[0]:pg = 'https://raidforums.com/' + n_page[0]
+            if 'https:/' not in n_page[0]:pg = 'https://raidforums.com/' + n_page
             yield Request(pg, callback = self.parse_forum)
-        threads_urls = response.xpath('//table[@class="tborder clear"]//tr//span[@class=" subject_new"]/a/@href').extract()
+        #threads_urls = response.xpath('//td[contains(@class,"thread-name")]/a/@href | //a[contains(@class,"thread-name")]/@href').extract()
+	threads_urls = response.xpath('//span[@class=" subject_new"]/a/@href').extract()
         for url_ in threads_urls:
             if 'https:/' not in url_:url = 'https://raidforums.com/'+ url_
 	    else:url = url_
